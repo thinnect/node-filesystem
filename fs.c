f7c227298a11a513add5abf6079fd940ccea46a3
Rebane <rebane@alkohol.ee>
2020-04-06 09:35:40 +0300

    multiple spiffs instances

--- fs.c
+++ fs.c
@@ -11,39 +11,88 @@
 #define __LOG_LEVEL__ (LOG_LEVEL_fs & BASE_LOG_LEVEL)
 #include "log.h"
 
-static volatile int fs_ready;
-static uint8_t fs_mount_count;
-static platform_mutex_t fs_mutex;
-static osMessageQueueId_t fs_queue;
-static spiffs_config fs_cfg;
-static spiffs fs_fs;
-static uint8_t fs_work_buf[SPIFFS_CFG_LOG_PAGE_SZ(0) * 2];
-static uint8_t fs_fds[32 * 4];
+#define FS_MAX                 2
+
+#define FS_SPIFFS_LOG_PAGE_SZ  (128UL)
+#define FS_SPIFFS_LOG_BLOCK_SZ (32UL * 1024UL)
+
+struct fs_struct{
+	fs_driver_t *driver;
+	volatile int ready;
+	int partition;
+	uint8_t mount_count;
+	platform_mutex_t mutex;
+	spiffs_config cfg;
+	spiffs fs;
+	uint8_t work_buf[FS_SPIFFS_LOG_PAGE_SZ * 2];
+	uint8_t fds[32 * 4];
+}fs[FS_MAX];
+
+osMessageQueueId_t fs_queue;
 
 static void fs_thread(void *p);
 
-void fs_init()
+static int32_t fs_read0(uint32_t addr, uint32_t size, uint8_t * dst);
+static int32_t fs_write0(uint32_t addr, uint32_t size, uint8_t * src);
+static int32_t fs_erase0(uint32_t addr, uint32_t size);
+static int32_t fs_read1(uint32_t addr, uint32_t size, uint8_t * dst);
+static int32_t fs_write1(uint32_t addr, uint32_t size, uint8_t * src);
+static int32_t fs_erase1(uint32_t addr, uint32_t size);
+static int32_t fs_read2(uint32_t addr, uint32_t size, uint8_t * dst);
+static int32_t fs_write2(uint32_t addr, uint32_t size, uint8_t * src);
+static int32_t fs_erase2(uint32_t addr, uint32_t size);
+
+void fs_init(int f, int partition, fs_driver_t *driver)
 {
-	fs_ready = 0;
-	fs_mount_count = 0;
-	fs_mutex = platform_mutex_new("fs");
+	fs[f].ready = 0;
+	fs[f].partition = partition;
+	fs[f].driver = driver;
+	fs[f].mount_count = 0;
+	fs[f].mutex = platform_mutex_new("fs");
+
+	fs[f].cfg.phys_size = driver->size(partition);
+	fs[f].cfg.phys_addr = 0;
+	fs[f].cfg.phys_erase_block = driver->erase_size(partition);
+	fs[f].cfg.log_block_size = FS_SPIFFS_LOG_BLOCK_SZ;
+	fs[f].cfg.log_page_size = FS_SPIFFS_LOG_PAGE_SZ;
+	if(f == 0){
+		fs[f].cfg.hal_read_f = fs_read0;
+		fs[f].cfg.hal_write_f = fs_write0;
+		fs[f].cfg.hal_erase_f = fs_erase0;
+#if FS_MAX > 0
+	}else if(f == 1){
+		fs[f].cfg.hal_read_f = fs_read1;
+		fs[f].cfg.hal_write_f = fs_write1;
+		fs[f].cfg.hal_erase_f = fs_erase1;
+#endif
+#if FS_MAX > 1
+	}else if(f == 2){
+		fs[f].cfg.hal_read_f = fs_read2;
+		fs[f].cfg.hal_write_f = fs_write2;
+		fs[f].cfg.hal_erase_f = fs_erase2;
+#endif
+	}
+}
+
+void fs_start()
+{
 	fs_queue = osMessageQueueNew(16, sizeof(void *), NULL);
 	osThreadNew(fs_thread, NULL, NULL);
 	// osMessageQueuePut(fs_queue, &fs_queue, 0, 0);
 }
 
-fs_fd fs_open(char *path, uint32_t flags)
+fs_fd fs_open(int f, char *path, uint32_t flags)
 {
 	spiffs_file sfd;
 	fs_fd fd;
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	spi_flash_lock();
-	debug1("open: %s", path);
-	sfd = SPIFFS_open(&fs_fs, path, flags, 0);
-	spi_flash_unlock();
-	fd = (fs_mount_count << 16) | sfd;
-	platform_mutex_release(fs_mutex);
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	fs[f].driver->lock();
+	debug1("open %d: %s", f, path);
+	sfd = SPIFFS_open(&fs[f].fs, path, flags, 0);
+	fs[f].driver->unlock();
+	fd = (fs[f].mount_count << 16) | sfd;
+	platform_mutex_release(fs[f].mutex);
 	if(sfd < 0)
 	{
 		return sfd;
@@ -51,152 +100,152 @@ fs_fd fs_open(char *path, uint32_t flags)
 	return fd;
 }
 
-int32_t fs_read(fs_fd fd, void *buf, int32_t len)
+int32_t fs_read(int f, fs_fd fd, void *buf, int32_t len)
 {
 	int32_t ret;
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	if(((fd >> 16) & 0xFF) != fs_mount_count)
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	if(((fd >> 16) & 0xFF) != fs[f].mount_count)
 	{
 		ret = -1;
 	}
 	else
 	{
-		spi_flash_lock();
-		ret = SPIFFS_read(&fs_fs, (fd & 0xFFFF), buf, len);
-		spi_flash_unlock();
+		fs[f].driver->lock();
+		ret = SPIFFS_read(&fs[f].fs, (fd & 0xFFFF), buf, len);
+		fs[f].driver->unlock();
 	}
-	platform_mutex_release(fs_mutex);
+	platform_mutex_release(fs[f].mutex);
 	return ret;
 }
 
-int32_t fs_write(fs_fd fd, const void *buf, int32_t len)
+int32_t fs_write(int f, fs_fd fd, const void *buf, int32_t len)
 {
 	int32_t ret;
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	if(((fd >> 16) & 0xFF) != fs_mount_count)
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	if(((fd >> 16) & 0xFF) != fs[f].mount_count)
 	{
 		ret = -1;
 	}
 	else
 	{
-		spi_flash_lock();
-		ret = SPIFFS_write(&fs_fs, (fd & 0xFFFF), (void *)buf, len);
-		spi_flash_unlock();
+		fs[f].driver->lock();
+		ret = SPIFFS_write(&fs[f].fs, (fd & 0xFFFF), (void *)buf, len);
+		fs[f].driver->unlock();
 	}
-	platform_mutex_release(fs_mutex);
+	platform_mutex_release(fs[f].mutex);
 	return ret;
 }
 
-int32_t fs_lseek(fs_fd fd, int32_t offs, int whence)
+int32_t fs_lseek(int f, fs_fd fd, int32_t offs, int whence)
 {
 	int32_t ret;
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	if(((fd >> 16) & 0xFF) != fs_mount_count)
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	if(((fd >> 16) & 0xFF) != fs[f].mount_count)
 	{
 		ret = -1;
 	}
 	else
 	{
-		spi_flash_lock();
-		ret = SPIFFS_lseek(&fs_fs, (fd & 0xFFFF), offs, whence);
-		spi_flash_unlock();
+		fs[f].driver->lock();
+		ret = SPIFFS_lseek(&fs[f].fs, (fd & 0xFFFF), offs, whence);
+		fs[f].driver->unlock();
 	}
-	platform_mutex_release(fs_mutex);
+	platform_mutex_release(fs[f].mutex);
 	return ret;
 }
 
-int32_t fs_fstat(fs_fd fd, fs_stat *s)
+int32_t fs_fstat(int f, fs_fd fd, fs_stat *s)
 {
 	int32_t ret;
 	spiffs_stat stat;
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	if(((fd >> 16) & 0xFF) != fs_mount_count)
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	if(((fd >> 16) & 0xFF) != fs[f].mount_count)
 	{
 		ret = -1;
 	}
 	else
 	{
-		spi_flash_lock();
-		ret = SPIFFS_fstat(&fs_fs, (fd & 0xFFFF), &stat);
-		spi_flash_unlock();
+		fs[f].driver->lock();
+		ret = SPIFFS_fstat(&fs[f].fs, (fd & 0xFFFF), &stat);
+		fs[f].driver->unlock();
 		s->size = stat.size;
 	}
-	platform_mutex_release(fs_mutex);
+	platform_mutex_release(fs[f].mutex);
 	return ret;
 }
 
-void fs_flush(fs_fd fd)
+void fs_flush(int f, fs_fd fd)
 {
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	if(((fd >> 16) & 0xFF) != fs_mount_count)
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	if(((fd >> 16) & 0xFF) != fs[f].mount_count)
 	{
 		warn1("stale fd");
 	}
 	else
 	{
-		spi_flash_lock();
-		SPIFFS_fflush(&fs_fs, (fd & 0xFFFF));
-		spi_flash_unlock();
+		fs[f].driver->lock();
+		SPIFFS_fflush(&fs[f].fs, (fd & 0xFFFF));
+		fs[f].driver->unlock();
 	}
-	platform_mutex_release(fs_mutex);
+	platform_mutex_release(fs[f].mutex);
 }
 
-void fs_close(fs_fd fd)
+void fs_close(int f, fs_fd fd)
 {
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	if(((fd >> 16) & 0xFF) != fs_mount_count)
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	if(((fd >> 16) & 0xFF) != fs[f].mount_count)
 	{
 		;
 	}
 	else
 	{
-		spi_flash_lock();
-		SPIFFS_close(&fs_fs, (fd & 0xFFFF));
-		spi_flash_unlock();
+		fs[f].driver->lock();
+		SPIFFS_close(&fs[f].fs, (fd & 0xFFFF));
+		fs[f].driver->unlock();
 	}
-	platform_mutex_release(fs_mutex);
+	platform_mutex_release(fs[f].mutex);
 }
 
-void fs_unlink(char *path)
+void fs_unlink(int f, char *path)
 {
-	platform_mutex_acquire(fs_mutex);
-	while(!fs_ready);
-	spi_flash_lock();
+	platform_mutex_acquire(fs[f].mutex);
+	while(!fs[f].ready);
+	fs[f].driver->lock();
 	debug1("unlink: %s", path);
-	SPIFFS_remove(&fs_fs, path);
-	spi_flash_unlock();
-	platform_mutex_release(fs_mutex);
+	SPIFFS_remove(&fs[f].fs, path);
+	fs[f].driver->unlock();
+	platform_mutex_release(fs[f].mutex);
 }
 
 static void fs_thread(void *p)
 {
+	int f;
 	int ret;
 	void *command;
 	uint32_t total, used;
-	fs_cfg.hal_erase_f = spi_flash_erase;
-	fs_cfg.hal_read_f = spi_flash_read;
-	fs_cfg.hal_write_f = spi_flash_write;
 	// spi_flash_mass_erase();
-	debug1("mounting");
-	spi_flash_lock();
-	ret = SPIFFS_mount(&fs_fs, &fs_cfg, fs_work_buf, fs_fds, sizeof(fs_fds), NULL, 0, 0);
-	if(ret != SPIFFS_OK)
-	{
-		debug1("formatting");
-		SPIFFS_format(&fs_fs);
-		SPIFFS_mount(&fs_fs, &fs_cfg, fs_work_buf, fs_fds, sizeof(fs_fds), NULL, 0, 0);
+	for (f = 0; f < FS_MAX; f++) {
+		debug1("mounting fs #%d", f);
+		fs[f].driver->lock();
+		ret = SPIFFS_mount(&fs[f].fs, &fs[f].cfg, fs[f].work_buf, fs[f].fds, sizeof(fs[f].fds), NULL, 0, 0);
+		if(ret != SPIFFS_OK)
+		{
+			debug1("formatting #%d", f);
+			SPIFFS_format(&fs[f].fs);
+			SPIFFS_mount(&fs[f].fs, &fs[f].cfg, fs[f].work_buf, fs[f].fds, sizeof(fs[f].fds), NULL, 0, 0);
+		}
+		ret = SPIFFS_info(&fs[f].fs, &total, &used);
+		fs[f].driver->unlock();
+		fs[f].mount_count++;
+		debug1("fs #%d ready, total: %u, used: %u", f, (unsigned int)total, (unsigned int)used);
+		fs[f].ready = 1;
 	}
-	ret = SPIFFS_info(&fs_fs, &total, &used);
-	spi_flash_unlock();
-	fs_mount_count++;
-	debug1("ready, total: %u, used: %u", (unsigned int)total, (unsigned int)used);
-	fs_ready = 1;
 	while(1)
 	{
 		if(osMessageQueueGet(fs_queue, &command, NULL, 1000) != osOK)continue;
@@ -250,4 +299,52 @@ static int fs_error_increase(int32_t error)
 	return(1);
 }
 */
+
+static int32_t fs_read0(uint32_t addr, uint32_t size, uint8_t * dst)
+{
+	return fs[0].driver->read(fs[0].partition, addr, size, dst);
+}
+
+static int32_t fs_write0(uint32_t addr, uint32_t size, uint8_t * src)
+{
+	return fs[0].driver->write(fs[0].partition, addr, size, src);
+}
+
+static int32_t fs_erase0(uint32_t addr, uint32_t size)
+{
+	return fs[0].driver->erase(fs[0].partition, addr, size);
+}
+
+#if FS_MAX > 0
+static int32_t fs_read1(uint32_t addr, uint32_t size, uint8_t * dst)
+{
+	return fs[1].driver->read(fs[1].partition, addr, size, dst);
+}
+
+static int32_t fs_write1(uint32_t addr, uint32_t size, uint8_t * src)
+{
+	return fs[1].driver->write(fs[1].partition, addr, size, src);
+}
+
+static int32_t fs_erase1(uint32_t addr, uint32_t size)
+{
+	return fs[1].driver->erase(fs[1].partition, addr, size);
+}
+#endif
+#if FS_MAX > 1
+static int32_t fs_read2(uint32_t addr, uint32_t size, uint8_t * dst)
+{
+	return fs[2].driver->read(fs[2].partition, addr, size, dst);
+}
+
+static int32_t fs_write2(uint32_t addr, uint32_t size, uint8_t * src)
+{
+	return fs[2].driver->write(fs[2].partition, addr, size, src);
+}
+
+static int32_t fs_erase2(uint32_t addr, uint32_t size)
+{
+	return fs[2].driver->erase(fs[2].partition, addr, size);
+}
+#endif
 
--- fs.h
+++ fs.h
@@ -24,6 +24,18 @@
 
 #define FS_ERR_REFORMATTED (-70000)
 
+struct fs_driver_struct{
+	int32_t(*read)(int partition, uint32_t addr, uint32_t size, uint8_t * dst);
+	int32_t(*write)(int partition, uint32_t addr, uint32_t size, uint8_t * src);
+	int32_t(*erase)(int partition, uint32_t addr, uint32_t size);
+	int32_t(*size)(int partition);
+	int32_t(*erase_size)(int partition);
+	void (*lock)();
+	void (*unlock)();
+};
+
+typedef struct fs_driver_struct fs_driver_t;
+
 typedef int32_t fs_fd;
 
 struct fs_stat_struct
@@ -34,83 +46,96 @@ struct fs_stat_struct
 typedef struct fs_stat_struct fs_stat;
 
 /**
- * Initializes LMT01 driver
+ * Initializes filesystem
  */
-void fs_init();
+void fs_init(int f, int partition, fs_driver_t *driver);
+
+/**
+ * Starts filesystem thread
+ */
+void fs_start();
 
 /**
  * Opens the file specified by path
  *
+ * @param f File system instance
  * @param path Name of the file to be opened
  * @param flags Flags
  *
  * @return Returns file descriptor or error
  */
-fs_fd fs_open(char *path, uint32_t flags);
+fs_fd fs_open(int f, char *path, uint32_t flags);
 
 /**
  * Attempts to read up to count bytes from file descriptor fd into the buffer starting at buf
  *
+ * @param f File system instance
  * @param fd File descriptor
  * @param buf Pointer where to read data
  * @param count Length of data to be read
  *
  * @return the number of bytes read or error
  */
-int32_t fs_read(fs_fd fd, void *buf, int32_t count);
+int32_t fs_read(int f, fs_fd fd, void *buf, int32_t count);
 
 /**
  * Writes up to count bytes from the buffer starting at buf
  *
+ * @param f File system instance
  * @param fd File descriptor
  * @param buf Pointer to the data to be written
  * @param count Length of data to be written
  *
  * @return the number of bytes written or error
  */
-int32_t fs_write(fs_fd fd, const void *buf, int32_t count);
+int32_t fs_write(int f, fs_fd fd, const void *buf, int32_t count);
 
 /**
  * Flushes cached writes to flash.
  *
+ * @param f File system instance
  * @param fd File descriptor
  */
-void fs_flush(fs_fd fd);
+void fs_flush(int f, fs_fd fd);
 
 /**
  * Closes the file descriptor
  *
+ * @param f File system instance
  * @param fd File descriptor
  */
-void fs_close(fs_fd fd);
+void fs_close(int f, fs_fd fd);
 
 /**
  * Deletes a name from the filesystem
  *
+ * @param f File system instance
  * @param path Name of the file to be deleted
  */
-void fs_unlink(char *path);
+void fs_unlink(int f, char *path);
 
 /**
  * Repositions the file offset of the open file descriptor
  *
+ * @param f File system instance
  * @param fd File descriptor
  * @param offs The new offset
  * @param whence Offset from beginning, from current location or from end of file
  *
  * @return The resulting offset or error
  */
-int32_t fs_lseek(fs_fd fd, int32_t offs, int whence);
+int32_t fs_lseek(int f, fs_fd fd, int32_t offs, int whence);
 
 /**
  * Return information about a file
  *
+ * @param f File system instance
  * @param fd File descriptor
  * @param s Buffer to store the information
  *
  * @return 0 or negative on error 
  */
-int32_t fs_fstat(fs_fd fd, fs_stat *s);
+int32_t fs_fstat(int f, fs_fd fd, fs_stat *s);
 
 #endif
 
