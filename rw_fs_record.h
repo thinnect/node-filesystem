6c8879d04e9a918fda633c488913edbf025f1294
Madis Uusjärv <madis@thinnect.com>
2020-09-28 14:50:31 +0300

    Added functions to read/write data structure from/to file

---
+++ rw_fs_record.h
@@ -0,0 +1,22 @@
+#ifndef RW_FS_RECORD_H_
+#define RW_FS_RECORD_H_
+
+/*****************************************************************************
+ * Write one data record to the file
+ * @params p_file_name - Pointer to the file name
+ * @params p_value - pointer to the data record
+ * @params len - data record length in bytes
+ * @return Returns number of bytes written on success, FILE_ERROR otherwise
+ ****************************************************************************/
+int32_t fs_write_record (const char * p_file_name, const void * p_value, int32_t len);
+
+/*****************************************************************************
+ * Read one data record from the file
+ * @params p_file_name - Pointer to the file name
+ * @params p_value - Pointer to the data record to read
+ * @params len - data record length in bytes
+ * @return Returns number of bytes read on success, FILE_ERROR otherwise
+ ****************************************************************************/
+int32_t fs_read_record (const char * p_file_name, void * p_value, int32_t len);
+
+#endif //RW_FS_RECORD_H_
